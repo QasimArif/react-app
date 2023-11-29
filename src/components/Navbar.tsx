0941895ae423c1c2632e976ef92de36f69daeca9
QasimArif <QasimArif@users.noreply.github.com>
2023-11-29 00:56:12 +0000

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav className="nav--main">
+            <div className="nav--logo">            
+                <img src="./src/assets/react.svg" alt=""  className="nav--image"/>
+                <h3 className="nav--logotitle">ReactFacts</h3>
+            </div>
+
+            <h4 className="nav--secondheading">React Course - Project 1</h4>
+
+        </nav>
+    )
+}
